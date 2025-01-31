ee4844daef1f2c6b95cbf99bac715a8fe9d2c65b
Mizumo-prjkt <bluepearl394@gmail.com>
2025-01-31 18:49:06 +0800

    Add device tree for Infinix X6532 with initial configuration files

---
+++ AndroidProducts.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_Infinix-X6532.mk
+
+# COMMON_LUNCH_CHOICES := \
+#     twrp_Infinix-X6532-user \
+#     twrp_Infinix-X6532-userdebug \
+#     twrp_Infinix-X6532-eng
+
+COMMON_LUNCH_CHOICES := \
+    twrp_Infinix-X6532-eng
+
